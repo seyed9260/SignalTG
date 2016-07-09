6f91dc51d222775a766196d8932e9e1d9e2ad4fb
MohammadReza <hackerfirefox@yahoo.com>
2016-07-09 14:31:05 +0430

    Add files via upload

--- data/cmd.sh
+++ data/cmd.sh
@@ -19,18 +19,17 @@ memUsed_m=$(($memTotal_m-$memFree_m-$memBuffer_m-$memCache_m))
 
 memUsedPrc=$((($memUsed_b*100)/$memTotal_b))
 
-echo "1⃣Memory: CRITICAL Total: $memTotal_m MB"
-echo "➖➖➖➖➖➖➖➖➖"
-echo "2⃣Memory Used: $memUsed_m MB - $memUsedPrc% used!"
-echo "➖➖➖➖➖➖➖➖➖"
-echo "3⃣Total : $memTotal_b"
-echo "➖➖➖➖➖➖➖➖➖"
-echo '4⃣CPU Usage : '"$CPUPer"'%'
-echo "➖➖➖➖➖➖➖➖➖"
-echo '5⃣Hdd : '"$hdd"'%'
-echo "➖➖➖➖➖➖➖➖➖"
-echo '6⃣Processes : '"$ProcessCnt"
-echo "➖➖➖➖➖➖➖➖➖"
-echo '7⃣Uptime : '"$uptime"
-echo "👑Signal Bot👑"
-echo "👑 @SignalTM 👑"
+echo "🔱 SUDO TM SERVER INFO:"
+echo "➖➖➖➖"
+echo "✅ Memory: $memUsed_m MB (4GB) \n💠 $memUsedPrc% used !"
+echo "➖➖"
+echo '✅ CPU Usage : '"$CPUPer"'%'
+echo "➖➖"
+echo '✅ HDD used : '"$hdd"'%'
+echo "➖➖"
+echo '✅ Processes : '"$ProcessCnt"
+echo "➖➖"
+echo '✅ Uptime : '"$uptime"
+echo "➖➖➖➖"
+echo "⭕️ SUDOTM !"
+echo ">> @sudo_Tm - @SUDOTM"
